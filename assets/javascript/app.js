4b476af212791dd1633d5c8475a49c9441b3ffec
Aarick-F <zeflyndesign@gmail.com>
2018-03-17 13:27:34 -0400

    added ajax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,24 @@
 
 //variables
-var search;
-var queryUrl;
+var searchTerm;
+var numRecords;
 var startDate;
 var endDate;
 
 // ajax
-
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "c6149b5cfe2845168c45e6f08920cfd5",
+  'q': searchTerm;
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
 
 
 // article counter
